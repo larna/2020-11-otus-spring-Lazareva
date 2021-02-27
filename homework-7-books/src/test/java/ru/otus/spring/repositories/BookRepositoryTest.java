37df754688854f6b6c60fd5d1d286ca15fa2d09c
Lazareva Nataly <larna@inbox.ru>
2021-02-27 13:50:14 +0300

    поправила тест

--- homework-7-books/src/test/java/ru/otus/spring/repositories/BookRepositoryTest.java
+++ homework-7-books/src/test/java/ru/otus/spring/repositories/BookRepositoryTest.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
-@DisplayName("Класс BookServiceImpl")
+@DisplayName("Класс BookRepository")
 @DataJpaTest
 class BookRepositoryTest {
     @Autowired
